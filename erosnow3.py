33aeee249c8cc8ca86542838b94f7c5c80d7c596
surabhi <surabhi0394@gmail.com>
2016-01-04 14:35:33 +0530

    taking filename and outputfile as arguments

--- erosnow3.py
+++ erosnow3.py
@@ -3,11 +3,14 @@ import sys
 import json
 # command line argument
 number = int(sys.argv[1])
-csvfile = open('erosnow.csv', 'r')
-jsonfile = open('file.json', 'w')
+name=sys.argv[2]
+outputfile=sys.argv[3]
+csvfile = open(name, 'r')
+jsonfile = open(outputfile, 'w')
 reader = csv.DictReader( csvfile, delimiter=',')
 out = json.dumps( [ row for row in reader ] , indent=4 )
 o = json.loads(out)  
 jsonfile.write(out)
 for i in range(number):
 	print(o[i])
+	
