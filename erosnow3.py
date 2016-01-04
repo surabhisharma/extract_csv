0f01ca534a841c1c87c865058f02c62d3960e9ca
surabhi <surabhi0394@gmail.com>
2016-01-04 16:44:52 +0530

    added the updated csv file and changed the script accordingly"

--- erosnow3.py
+++ erosnow3.py
@@ -1,13 +1,14 @@
 import csv
 import sys
+import os
 import json
 # command line argument
 number = int(sys.argv[1])
 name=sys.argv[2]
 outputfile=sys.argv[3]
-csvfile = open(name, 'r')
+csvfile = open(name, 'rU')
 jsonfile = open(outputfile, 'w')
-reader = csv.DictReader( csvfile, delimiter=',')
+reader = csv.DictReader( csvfile, delimiter=',',dialect=csv.excel_tab)
 out = json.dumps( [ row for row in reader ] , indent=4 )
 o = json.loads(out)  
 jsonfile.write(out)
